4d2900309fa2ade310cf460880d37903e54f3867
adamsjs23 <adamaguebre@yahoo.fr>
2024-07-29 10:52:40 +0200

    Selecting and manipulating elements

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 25;
+console.log(document.querySelector('.guess').value);
