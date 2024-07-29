1d68d79282759c9f2a01a1b4a37231d47c240c16
adamsjs23 <adamaguebre@yahoo.fr>
2024-07-29 12:22:25 +0200

    Challenge #7.1 completed

--- script.js
+++ script.js
@@ -9,9 +9,9 @@
 
 // document.querySelector('.guess').value = 25;
 // console.log(document.querySelector('.guess').value);
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+
 console.log('The number to guess', secretNumber);
 document.querySelector('.check').addEventListener('click', function (evt) {
   const guess = Number(document.querySelector('.guess').value);
@@ -19,6 +19,7 @@ document.querySelector('.check').addEventListener('click', function (evt) {
   if (!guess) {
     document.querySelector('.message').textContent = 'No number';
   } else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.message').textContent =
       'Correct Number, You win !!!!';
     document.querySelector('body').style.backgroundColor = '#60b347';
@@ -41,4 +42,19 @@ document.querySelector('.check').addEventListener('click', function (evt) {
     }
   }
   document.querySelector('.score').textContent = score;
+});
+
+document.querySelector('.again').addEventListener('click', () => {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  console.log(secretNumber)
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.guess').value = '';
+
+  // 
+  document.querySelector('.number').textContent = '?';
+    document.querySelector('.message').textContent =
+      'Start guessing ...';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
 });
