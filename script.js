3e430e2c73fa1d12da709d87f21a0ca9e0852232
adamsjs23 <adamaguebre@yahoo.fr>
2024-07-29 11:04:39 +0200

    Handling click events

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = 'Correct number!';
-console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct number!';
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 20;
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20;
 
-document.querySelector('.guess').value = 25;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 25;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function (evt) {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number';
+  }
+});
