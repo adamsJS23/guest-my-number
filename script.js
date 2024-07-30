faee3113c6c986ba9f15f1a2287aa2cf63cb0550
adamsjs23 <adamaguebre@yahoo.fr>
2024-07-30 11:25:04 +0200

    Implemented high score functionality

--- script.js
+++ script.js
@@ -11,19 +11,26 @@
 // console.log(document.querySelector('.guess').value);
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
-
+let lastScore = 0;
+let highScore = 0;
+let isGameStarted = false;
+let isGamerWinsOnce = false;
 console.log('The number to guess', secretNumber);
 document.querySelector('.check').addEventListener('click', function (evt) {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
   if (!guess) {
     document.querySelector('.message').textContent = 'No number';
   } else if (guess === secretNumber) {
+    if (score > lastScore) {
+      lastScore = score;
+      document.querySelector('.highscore').textContent = score;
+    }
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.message').textContent =
       'Correct Number, You win !!!!';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    isGamerWinsOnce = true;
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too high';
@@ -42,19 +49,23 @@ document.querySelector('.check').addEventListener('click', function (evt) {
     }
   }
   document.querySelector('.score').textContent = score;
+  isGameStarted = true;
 });
 
 document.querySelector('.again').addEventListener('click', () => {
+  if (lastScore >= highScore) {
+    highScore = lastScore;
+    document.querySelector('.highscore').textContent = highScore;
+  }
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20 + 1);
-  console.log(secretNumber)
+  console.log('Number to guest', secretNumber);
   document.querySelector('.score').textContent = score;
   document.querySelector('.guess').value = '';
 
-  // 
+  //
   document.querySelector('.number').textContent = '?';
-    document.querySelector('.message').textContent =
-      'Start guessing ...';
-    document.querySelector('body').style.backgroundColor = '#222';
-    document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.message').textContent = 'Start guessing ...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
 });
